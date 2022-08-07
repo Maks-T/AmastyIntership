fc345bcee1806b5d005a5d790c22d727cdc949b2
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-08-07 12:35:50 +0300

    feat: add ResultFactory to /Controller/Index/Index.php

--- TsatsuraModule/Controller/Index/Index.php
+++ TsatsuraModule/Controller/Index/Index.php
@@ -4,24 +4,21 @@ namespace Amasty\TsatsuraModule\Controller\Index;
 
 
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index implements ActionInterface {
+
+    /**
+     * @var ResultFactory
+     */
+    private ResultFactory $resultFactory;
+
+    public function __construct(ResultFactory $resultFactory) {
+       $this->resultFactory = $resultFactory;
+    }
+
     public function execute()
     {
-        $style = '"
-        display: flex; 
-        justify-content: center; 
-        align-items: center; 
-        margin-top: 20%; 
-        text-shadow: 4px 2px 1px rgba(144, 150, 150, 0.47); 
-        color: blue;
-        font-family: cursive;
-        text-align: center;
-        font-size: 3rem;
-        "';
-
-        $message = "<h1 style={$style}>Привет Magento. Привет Amasty. Я готов тебя покорить!</h1>";
-
-        die($message);
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
