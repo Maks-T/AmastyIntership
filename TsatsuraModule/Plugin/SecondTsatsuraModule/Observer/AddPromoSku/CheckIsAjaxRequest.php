6b20bfa0a7337cbed84b9d0449a1e18e11c82fb4
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-08-23 22:28:23 +0300

    feat: CheckIsAjaxRequest

---
+++ TsatsuraModule/Plugin/SecondTsatsuraModule/Observer/AddPromoSku/CheckIsAjaxRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Amasty\TsatsuraModule\Plugin\SecondTsatsuraModule\Observer\AddPromoSku;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Message\ManagerInterface;
+
+class CheckIsAjaxRequest
+{
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * @var ManagerInterface
+     */
+    private $messageManager;
+
+    public function __construct(
+        RequestInterface $request,
+        ManagerInterface $messageManager
+    ) {
+        $this->request = $request;
+        $this->messageManager = $messageManager;
+    }
+
+    public function aroundExecute($subject, callable $proceed, $observer)
+    {
+        if (!$this->request->isAjax()) {
+            return $proceed($observer);
+        } else {
+            $this->messageManager->addWarningMessage('Ajax request sent! Promo product isn\'t added');
+        }
+    }
+}
