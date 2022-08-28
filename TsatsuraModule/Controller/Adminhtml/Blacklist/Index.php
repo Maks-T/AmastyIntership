38f7e3574e143de140ecdaa65daad58dde5d69db
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-08-28 16:25:46 +0300

    feat: add menu item admin

---
+++ TsatsuraModule/Controller/Adminhtml/Blacklist/Index.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Amasty\TsatsuraModule\Controller\Adminhtml\Blacklist;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index implements ActionInterface
+{
+     /**
+     * @var ResultFactory
+     */
+    private $resultFactory;
+
+    public function __construct(
+        ResultFactory $resultFactory,
+    ) {
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->prepend(__('Blacklist'));
+
+        return $resultPage;
+    }
+}
